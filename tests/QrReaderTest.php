6408c20dd70b37972f63f003580ef328707e5bb6
Meindert-Jan Kroese <mkroese@eljakim.nl>
2021-01-08 20:09:38 +0100

    fix TypeError under php8.0 ($this->result can be false)

--- tests/QrReaderTest.php
+++ tests/QrReaderTest.php
@@ -15,4 +15,11 @@ class QrReaderTest extends TestCase
         $qrcode = new QrReader($image);
         $this->assertSame("Hello world!", $qrcode->text());
     }
+
+    public function testNoText()
+    {
+        $image = __DIR__ . "/qrcodes/empty.png";
+        $qrcode = new QrReader($image);
+        $this->assertSame(false, $qrcode->text());
+    }
 }
